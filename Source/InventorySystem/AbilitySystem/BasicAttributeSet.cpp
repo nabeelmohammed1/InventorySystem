62e7301cd194a38518fddb59c88f12aa7fa64c24
Nabeel Mohammed <64881066+nabeelmohammed1@users.noreply.github.com>
2026-09-22 13:09:49 -0400

    basic hud UI setup and setting up equipment to actual affect stats

--- Source/InventorySystem/AbilitySystem/BasicAttributeSet.cpp
+++ Source/InventorySystem/AbilitySystem/BasicAttributeSet.cpp
@@ -7,4 +7,8 @@ UBasicAttributeSet::UBasicAttributeSet()
 {
 	Health = 100.f;
 	MaxHealth = 100.f;
+	MovementSpeedMultiplier = 1.f;
+	Armor = 0.f;
+	Strength = 1.f;
+	CarryingCapacityMultiplier = 1.f;
 }
